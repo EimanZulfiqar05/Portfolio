5c46a98a4a40c278b4f3340a4f321295db02b48e
Wix Remote Machine <support@wix.com>
2026-03-04 15:29:57 +0000

    Change logo title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -56,7 +56,7 @@ export default function Header() {
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
             >
-              EmanZ
+              BuildWithEman
             </motion.a>
 
             {/* Desktop Navigation */}
